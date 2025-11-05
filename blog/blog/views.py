c751b152d1462bab635f6892f4b2c2785f3ce433
ji-min0 <k.jimin2002@gmail.com>
2025-11-05 14:10:47 +0900

    :sparkles: feat: 3일차(쿠키, 세션)

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -5,8 +5,19 @@ from blog.models import Blog
 
 def blog_list(request):
     blogs = Blog.objects.all()
+    visits = int(request.COOKIES.get('visits', 0)) + 1
 
-    return render(request, 'blog_list.html', {'blogs': blogs})
+    request.session['count'] = request.session.get('count', 0) + 1
+
+    context = {
+        'blogs': blogs,
+        'count': request.session['count'],
+    }
+
+    response = render(request, 'blog_list.html', context)
+    response.set_cookie('visits', visits)
+
+    return response
 
 def blog_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
