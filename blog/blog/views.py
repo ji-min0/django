f33aa07cd8499b0ecda7c179700e3c3612b5bb1b
ji-min0 <k.jimin2002@gmail.com>
2025-11-05 14:43:21 +0900

    :sparkles: feat: 3일차(로그아웃)

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -5,17 +5,17 @@ from blog.models import Blog
 
 def blog_list(request):
     blogs = Blog.objects.all()
-    visits = int(request.COOKIES.get('visits', 0)) + 1
+    # visits = int(request.COOKIES.get('visits', 0)) + 1
 
-    request.session['count'] = request.session.get('count', 0) + 1
+    # request.session['count'] = request.session.get('count', 0) + 1
 
     context = {
         'blogs': blogs,
-        'count': request.session['count'],
+        # 'count': request.session['count'],
     }
 
     response = render(request, 'blog_list.html', context)
-    response.set_cookie('visits', visits)
+    # response.set_cookie('visits', visits)
 
     return response
 
