fd77d235ad7a1f1e6f16310bcd6f430e96542061
ji-min0 <k.jimin2002@gmail.com>
2025-11-12 23:31:44 +0900

    :sparkles: feat: 8일차 과제

--- todo/config/urls.py
+++ todo/config/urls.py
@@ -14,24 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import todo_list, todo_info, todo_create, todo_delete, todo_update
-from users import views as user_views
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('todo/', todo_list, name='todo_list'),
+    path('todo/create/', todo_create, name='todo_create'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
     path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
-    path('todo/create/', todo_create, name='todo_create'),
-
+    path('admin/', admin.site.urls),
     # auth
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', user_views.login, name='login'),
-    path('accounts/signup', user_views.sign_up, name='signup'),
-
-    # cbv
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/login/', user_views.login, name='login'),
+    # path('accounts/signup/', user_views.sign_up, name='signup'),
+    path('users/',  include('users.urls')),
+    # CBV URL include
     path('cbv/', include('todo.urls')),
+    # summernote URL include
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
